05009cc893597d2d2d44599ba00067cb50178337
Shrikar Patel <shrikarpatel98@gmail.com>
2022-09-02 12:25:25 -0400

    add build

---
+++ build/precache-manifest.c85311a6b7643919689080ca70b5b6c3.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a5273ded0a3b16ef275f24000815b3c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "412725bf1201a5e138b6",
+    "url": "/portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "63a31e9ba04a1f0d6a57",
+    "url": "/portfolio/static/css/main.0a34ba22.chunk.css"
+  },
+  {
+    "revision": "412725bf1201a5e138b6",
+    "url": "/portfolio/static/js/2.02ac2210.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.02ac2210.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63a31e9ba04a1f0d6a57",
+    "url": "/portfolio/static/js/main.919b9d9a.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "b0d4bae890ace40cf8e8b9f72270bbc7",
+    "url": "/portfolio/static/media/binary_background.b0d4bae8.jpg"
+  },
+  {
+    "revision": "8a645f63da8d78dafa6a03f510607e0f",
+    "url": "/portfolio/static/media/chess.8a645f63.png"
+  },
+  {
+    "revision": "160c6d2a51d646751c8000cfb2157e2a",
+    "url": "/portfolio/static/media/hackhers.160c6d2a.png"
+  },
+  {
+    "revision": "ff157bfc9c8cf6063f3e0e7c177e8910",
+    "url": "/portfolio/static/media/hackhers20.ff157bfc.png"
+  },
+  {
+    "revision": "82bb369c2f20e18138460dc8b1a4a323",
+    "url": "/portfolio/static/media/hackru.82bb369c.png"
+  },
+  {
+    "revision": "7d34e6b22b455421167bb29da438a6b3",
+    "url": "/portfolio/static/media/hackru2019.7d34e6b2.png"
+  },
+  {
+    "revision": "051a223c64454436110508cb2f6477fa",
+    "url": "/portfolio/static/media/memeify-me.051a223c.jpg"
+  },
+  {
+    "revision": "e6f14146cc3b8b26ac2b55b31ad4ec3b",
+    "url": "/portfolio/static/media/nba.e6f14146.png"
+  },
+  {
+    "revision": "38752e40dd79da05acf88c437cf7a7ad",
+    "url": "/portfolio/static/media/remind.38752e40.png"
+  },
+  {
+    "revision": "246bfb77c27862f979dff874fe473da0",
+    "url": "/portfolio/static/media/rutgers.246bfb77.png"
+  },
+  {
+    "revision": "d7242cbd70645892bf23084101321927",
+    "url": "/portfolio/static/media/trakpack.d7242cbd.png"
+  },
+  {
+    "revision": "3270523620eb33e53af065b322a099f8",
+    "url": "/portfolio/static/media/venmore.32705236.jpg"
+  }
+]);
